3825f5db128816f8079f85ccfc45d374c130e99e
CAT-i0n <rudman.iv.o@gmail.com>
2023-08-11 16:48:21 +0300

    add initial configuration

---
+++ .config/waybar/scripts/powermenu.sh
@@ -0,0 +1,6 @@
+rofi \
+  -show p \
+  -theme Paper \
+  -font "FiraCode Nerd Font 20" \
+  -modi p:'rofi-power-menu --symbols-font "FiraCode Nerd Font 20"' \
+  -theme-str 'window {width: 8em;} listview {lines: 6;}'
