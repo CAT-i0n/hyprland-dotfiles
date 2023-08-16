e0e554e4f5e322b7437120be42c40731d2f8e207
CAT-i0n <rudman.iv.o@gmail.com>
2023-08-16 15:56:46 +0300

    add autologin to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec Hyprland && swaylock
+    end
+end
