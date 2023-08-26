09accd66470bb2b103974592cf7312f5c81221e3
CAT-i0n <rudman.iv.o@gmail.com>
2023-08-26 21:41:57 +0300

    change rofi theme

---
+++ .config/hypr/scripts/powermenu.sh
@@ -0,0 +1,5 @@
+rofi \
+  -show p \
+  -theme "../rofi/tokyonight.rasi" \
+  -modi p:'rofi-power-menu --symbols-font "FiraCode Nerd Font 25"' \
+  -theme-str 'window {width: 18em;} listview {lines: 6;}'
--- .config/waybar/scripts/powermenu.sh
+++
@@ -1,6 +0,0 @@
-rofi \
-  -show p \
-  -theme Paper \
-  -font "FiraCode Nerd Font 20" \
-  -modi p:'rofi-power-menu --symbols-font "FiraCode Nerd Font 20"' \
-  -theme-str 'window {width: 8em;} listview {lines: 6;}'
